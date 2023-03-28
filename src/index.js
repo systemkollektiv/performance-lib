13f19995ea1c022bec54848da4e1cd1ccab2a7a4
jaeh <jascha@jaeh.at>
2023-03-28 15:40:11 +0200

    src/index: export config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { osc as o } from './servers/osc.js'
 import { ws as w } from './servers/ws.js'
+import * as c from './config.js'
 
 export const osc = o
 export const ws = w
+export const config = c
 
 export default {
   osc,
   ws,
+  config,
 }
