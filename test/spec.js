26d65888675692620cf673442d972dd7ff692f30
jaeh <jascha@jaeh.at>
2023-04-01 18:47:10 +0200

    update test/spec, still incomplete

--- test/spec.js
+++ test/spec.js
@@ -1,10 +1,22 @@
 import { version } from '@magic/test'
 
-import lib, { osc, ws } from '../src/index.js'
+import lib, { config, osc, ws } from '../src/index.js'
 
 const spec = {
   osc: 'function',
   ws: 'function',
+  config: {
+    addresses: 'array',
+    obsWebsocketConfig: {},
+    outDir: 'string',
+    performancePlayerOscConfig: {},
+    performanceRecorderOscConfig: {},
+    performanceUiOscConfig: {},
+    testClientOscConfig: {
+      localAddress: 'string',
+      localPort: 'string',
+    },
+  },
 }
 
-export default [...version(lib, spec), ...version({ osc, ws }, spec)]
+export default [...version(lib, spec), ...version({ config, osc, ws }, spec)]
