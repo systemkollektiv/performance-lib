98d07cd3ee41f20f96bb00ae060509dd361d7cad
jaeh <jascha@jaeh.at>
2023-04-05 19:50:23 +0200

    src/servers/obs: try reconnect before starting presenter

--- src/servers/obs.js
+++ src/servers/obs.js
@@ -1,15 +1,25 @@
+import log from '@magic/log'
+
 import OBSWebSocket from 'obs-websocket-js'
 
-export const obs = async parent => {
+export const obs = async (parent, resolve) => {
   const { obsConfig, obsEventSubscriptions = 0 } = parent
-
-  const obsWebSocket = new OBSWebSocket()
-
   const url = `${obsConfig.protocol}://${obsConfig.address}:${obsConfig.port}`
 
-  await obsWebSocket.connect(url, obsConfig.password, {
-    eventSubscriptions: obsEventSubscriptions,
-  })
+  try {
+    const obsWebSocket = new OBSWebSocket()
 
-  return obsWebSocket
+    await obsWebSocket.connect(url, obsConfig.password, {
+      eventSubscriptions: obsEventSubscriptions,
+    })
+
+    console.log('Connected to obs via', url)
+
+    resolve(obsWebSocket)
+  } catch (e) {
+    const waitForRetryDuration = 5_000
+    log.warn('W_OBS_CONNECTION', e.message)
+    log.info('obs refused the connection to', url, 'retrying in', waitForRetryDuration / 1000, 'seconds')
+    setTimeout(() => obs(parent, resolve), waitForRetryDuration)
+  }
 }
