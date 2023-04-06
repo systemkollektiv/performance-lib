35c36e69551eea1bf4bc997d5117807c4da72322
jaeh <jascha@jaeh.at>
2023-04-06 19:51:45 +0200

    format

--- src/servers/obs.js
+++ src/servers/obs.js
@@ -2,25 +2,31 @@ import log from '@magic/log'
 
 import OBSWebSocket from 'obs-websocket-js'
 
-export const obs = async parent => new Promise(async resolve => {
+export const obs = async parent =>
+  new Promise(async resolve => {
+    const { obsConfig, obsEventSubscriptions = 0 } = parent
+    const url = `${obsConfig.protocol}://${obsConfig.address}:${obsConfig.port}`
 
-  const { obsConfig, obsEventSubscriptions = 0 } = parent
-  const url = `${obsConfig.protocol}://${obsConfig.address}:${obsConfig.port}`
+    try {
+      const obsWebSocket = new OBSWebSocket()
 
-  try {
-    const obsWebSocket = new OBSWebSocket()
+      await obsWebSocket.connect(url, obsConfig.password, {
+        eventSubscriptions: obsEventSubscriptions,
+      })
 
-    await obsWebSocket.connect(url, obsConfig.password, {
-      eventSubscriptions: obsEventSubscriptions,
-    })
+      console.log('Connected to obs via', url)
 
-    console.log('Connected to obs via', url)
-
-    resolve(obsWebSocket)
-  } catch (e) {
-    const waitForRetryDuration = 5_000
-    log.warn('W_OBS_CONNECTION', e.message)
-    log.info('obs refused the connection to', url, 'retrying in', waitForRetryDuration / 1000, 'seconds')
-    setTimeout(() => obs(parent, resolve), waitForRetryDuration)
-  }
-})
+      resolve(obsWebSocket)
+    } catch (e) {
+      const waitForRetryDuration = 5_000
+      log.warn('W_OBS_CONNECTION', e.message)
+      log.info(
+        'obs refused the connection to',
+        url,
+        'retrying in',
+        waitForRetryDuration / 1000,
+        'seconds',
+      )
+      setTimeout(() => obs(parent, resolve), waitForRetryDuration)
+    }
+  })
